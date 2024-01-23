67fed890eb998b4087a98fffdd65bb000d944a6a
HimitsuHito <95978915+HimitsuHito@users.noreply.github.com>
2024-01-23 20:00:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const oneYearFromNow = new Date(d.setFullYear(d.getFullYear() + 1));
 
 let birthday = "";
 
-checkPastOrFuture("3", "march");
+checkPastOrFuture("23", "january");
 
 function submitDate(e) {
   let year, str;
